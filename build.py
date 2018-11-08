b85efd694ca905f806a6052398ccbf408ab0936d
k000 <AtaLuZiK@gmail.com>
2018-11-09 00:50:10 +0800

    disable shared_option_name

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(shared_option_name=False)
 
     if platform.system() != "Windows":
         extend_settings = []
